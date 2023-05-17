1a478d53fdb90808deafe552091af37cbb346cb1
dcdnce <pforesti@student.42nice.fr>
2023-05-17 18:35:52 +0200

    root flex soon 😇

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+const div__mousemove: HTMLElement | null = document.getElementById('div__mousemove');
+
+window.addEventListener('mousemove', (e: MouseEvent) => {
+	if (div__mousemove == null)
+		return ;
+	div__mousemove.style.top = e.pageY + "px";
+	div__mousemove.style.left = e.pageX + "px";
+});
+
+window.addEventListener('mousedown', (e: MouseEvent) => {
+	if (div__mousemove == null)
+		return ;
+	div__mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+});
+
+window.addEventListener('mouseup', (e: MouseEvent) => {
+	if (div__mousemove == null)
+		return ;
+	div__mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+})
