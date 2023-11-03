b9a4c044a821302fa73e098c8596b93a237c5af7
dcdnce <pforesti@student.42nice.fr>
2023-11-03 18:27:07 +0100

    🎨 link added and other qol

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,18 @@
 "use strict";
-const div__mousemove = document.getElementById('div__mousemove');
+const mousemove = document.getElementById('mousemove');
 window.addEventListener('mousemove', (e) => {
-    if (div__mousemove == null)
+    if (mousemove == null)
         return;
-    div__mousemove.style.top = e.pageY + "px";
-    div__mousemove.style.left = e.pageX + "px";
+    mousemove.style.top = e.pageY + "px";
+    mousemove.style.left = e.pageX + "px";
 });
 window.addEventListener('mousedown', (e) => {
-    if (div__mousemove == null)
+    if (mousemove == null)
         return;
-    div__mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+    mousemove.style.transform = "scale(2) translate(-25%, -25%)";
 });
 window.addEventListener('mouseup', (e) => {
-    if (div__mousemove == null)
+    if (mousemove == null)
         return;
-    div__mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+    mousemove.style.transform = "scale(1) translate(-50%, -50%)";
 });
